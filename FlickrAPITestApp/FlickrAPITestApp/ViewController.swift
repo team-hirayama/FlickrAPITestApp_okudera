951b324142525930b9dde679487a4e4efcad141e
yuoku <yuoku0708@gmail.com>
2016-10-22 13:32:13 +0900

    add directory作成

---
+++ FlickrAPITestApp/FlickrAPITestApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlickrAPITestApp
+//
+//  Created by OkuderaYuki on 2016/10/22.
+//  Copyright © 2016年 yuoku. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
