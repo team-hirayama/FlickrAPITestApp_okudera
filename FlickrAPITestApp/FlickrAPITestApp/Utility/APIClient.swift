c0a22fc084c0c7e541ea784b3ff3c0e1358a1d38
yuoku <yuoku0708@gmail.com>
2016-10-22 13:48:09 +0900

    add APIメソッド作成

---
+++ FlickrAPITestApp/FlickrAPITestApp/Utility/APIClient.swift
@@ -0,0 +1,42 @@
+//
+//  APIClient.swift
+//  FlickrAPITestApp
+//
+//  Created by OkuderaYuki on 2016/10/22.
+//  Copyright © 2016年 yuoku. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class APIClient: NSObject {
+    enum Result {
+        case Success(Any)
+        case Error(Error)
+    }
+    
+    /// 汎用APIメソッド(GET)
+    class func requestItems(urlString: String,
+                            params: [String: Any] = [String: Any](),
+                            headers: [String: Any] = [String: Any](),
+                            completionHandler: @escaping (Result) -> () = {_ in})
+    {
+        let request = Alamofire.request(urlString, method: .get, parameters: params)
+        request.responseJSON { (response: DataResponse) in
+            let result = response.result
+            
+            if result.isSuccess {
+                guard let value = result.value else {
+                    return
+                }
+                completionHandler(.Success(value))
+            } else {
+                guard let error = result.error else {
+                    return
+                }
+                completionHandler(.Error(error))
+            }
+        }
+    }
+}
+
--- FlickrAPITestApp/FlickrAPITestApp/ViewController/SearchPhotosViewController.swift
+++ FlickrAPITestApp/FlickrAPITestApp/ViewController/SearchPhotosViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SearchPhotosViewController.swift
 //  FlickrAPITestApp
 //
 //  Created by OkuderaYuki on 2016/10/22.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SearchPhotosViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
